bc6ec404263fa37dffcbcc819c941381bd3d18fd
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-11-28 11:02:05 +0000

    Create a caching mechanism for predictors

---
+++ app/classes/predictors/cache.rb
@@ -0,0 +1,34 @@
+class Predictors::Cache
+  
+  DefaultExpiry = 24.hours
+  
+  def self.get_predictor_for(user)
+    predictor = Redis.current.get(cache_key_for(user))
+    if predictor
+      predictor = Marshal.load(predictor)
+    else
+      predictor = build_predictor_for(user)
+      set_predictor_for(user, predictor)
+    end
+    predictor
+  end
+  
+  def self.set_predictor_for(user, predictor, expire_after = DefaultExpiry)
+    Redis.current.setex(cache_key_for(user), expire_after, Marshal.dump(predictor))
+  end
+  
+  def self.build_predictor_for(user)
+    Predictors::IB1.train_for(user)
+  end
+  
+  def self.clear_for!(user)
+    Redis.current.del(cache_key_for(user))
+  end
+  
+  protected
+  
+  def self.cache_key_for(user)
+    "user:#{ user.id }:predictor"
+  end
+
+end
