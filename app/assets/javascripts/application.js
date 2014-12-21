fe068939baee98756d8883b55475bbc7fe24fd7a
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-12-21 09:46:43 +0000

    Remove deprecated refresh code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,64 +17,6 @@
 //= require_tree .
 
 
-// Page Refresh Timer - used for starting and stopping page refreshes.
-var page_refresh_timer = null;
-
-// Page refresh interval, in milliseconds.
-var page_refresh_interval = 15 * 1000;
-
-function hasAutoRefresh() {
-  return page_refresh_timer != null;
-};
-
-function toggleAutorefresh() {
-  if (hasAutoRefresh()) {
-    stopAutoRefresh();
-  } else {
-    startAutoRefresh();
-  }
-};
-
-function updateAutorefreshButton() {
-
-  if (hasAutoRefresh()) {
-    $("#autorefresh").addClass('active');
-    $("#autorefresh").removeClass('hidden');
-  } else {
-    $("#autorefresh").removeClass('active');
-  }
-
-  console.log("Toggling refresh button: " + $('#autorefresh').hasClass('active'));
-};
-
-// Start page refreshes
-function startAutoRefresh() {
-  stopAutoRefresh();
-  page_refresh_timer = setTimeout(doAutoRefresh, page_refresh_interval);
-  console.log("Started auto refresh timer.");
-  updateAutorefreshButton();
-};
-
-// Stop page refreshes
-function stopAutoRefresh() {
-  if (page_refresh_timer != null) {
-    console.log("Stopping auto refresh timer.")
-    clearTimeout(page_refresh_timer);
-    page_refresh_timer = null;
-  }
-  updateAutorefreshButton();
-};
-
-// Perform a page refresh
-function doAutoRefresh() {
-  console.log("Performing auto refresh.")
-  page_refresh_timer = null;
-  Turbolinks.visit(location.toString());
-};
-
-// Always turn off page refreshes when moving to a new page
-$(document).on("page:before-change", stopAutoRefresh);
-
 function geolocateMe() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(submitSearch, showError);
