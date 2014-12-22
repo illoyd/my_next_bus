5764bbb76981890224b63c6ccfad52e0b1ad5019
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-12-22 15:08:51 +0000

    Add parameter for MLP

--- app/classes/predictors/multilayer_perceptron.rb
+++ app/classes/predictors/multilayer_perceptron.rb
@@ -2,6 +2,7 @@ class Predictors::MultilayerPerceptron < Predictors::Predictor
   
   def train(dataset)
     @predictor = Ai4r::Classifiers::MultilayerPerceptron.new
+    @predictor.set_parameters(:hidden_layers, [8,6])
     @predictor.build(dataset)
     self
   end
