122bc0086c74fe0217bc84278d40ee25ce527cbd
Ian Lloyd <ian.w.lloyd@gmail.com>
2015-03-15 11:25:21 +0000

    Save towards from stop data

--- app/jobs/create_or_update_stop_job.rb
+++ app/jobs/create_or_update_stop_job.rb
@@ -3,9 +3,10 @@ class CreateOrUpdateStopJob < ActiveJob::Base
   def perform(city, stop)
     stop = NotTfL::Stop.new(stop)
 
-    TransitStop.find_or_create_by(stop_sid: stop.code1, city: city).tap do |ss|
+    TransitStop.find_or_initialize_by(stop_sid: stop.code1, city: city).tap do |ss|
       ss.name        = stop.name
       ss.indicator   = stop.indicator
+      ss.towards   ||= stop.towards
       ss.latitude  ||= stop.latitude
       ss.longitude ||= stop.longitude
       ss.save!
