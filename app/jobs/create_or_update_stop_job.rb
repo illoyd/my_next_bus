6507a569e06985b1c0046773e5223ebe7a5d0f99
Ian Lloyd <ian.w.lloyd@gmail.com>
2015-03-15 12:14:41 +0000

    Defensive updates to stops
    Don’t update stop fields unless present

--- app/jobs/create_or_update_stop_job.rb
+++ app/jobs/create_or_update_stop_job.rb
@@ -4,8 +4,8 @@ class CreateOrUpdateStopJob < ActiveJob::Base
     stop = NotTfL::Stop.new(stop)
 
     TransitStop.find_or_initialize_by(stop_sid: stop.code1, city: city).tap do |ss|
-      ss.name        = stop.name
-      ss.indicator   = stop.indicator
+      ss.name      ||= stop.name
+      ss.indicator ||= stop.indicator
       ss.towards   ||= stop.towards
       ss.latitude  ||= stop.latitude
       ss.longitude ||= stop.longitude
